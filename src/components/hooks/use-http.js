facb472184f8c6b859d740c5b28cbf046fa3e4be
Richard <haggio0844@gmail.com>
2022-06-08 22:04:47 +0200

    useHttp🪝

---
+++ src/components/hooks/use-http.js
@@ -0,0 +1,36 @@
+import { useCallback, useState } from 'react';
+
+const useHttp = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	const sendRequest = useCallback(async (config, applyData) => {
+		setIsLoading(true);
+		setError(null);
+		try {
+			const response = await fetch(config.url, {
+				method: config.method ? config.method : 'GET',
+				body: config.body ? JSON.stringify(config.body) : null,
+				headers: config.header ? config.header : {},
+			});
+
+			if (!response.ok) {
+				throw new Error('Request failed!');
+			}
+
+			const data = await response.json();
+			applyData(data);
+		} catch (err) {
+			setError(err.message || 'Something went wrong!');
+		}
+		setIsLoading(false);
+	}, []);
+
+	return {
+		isLoading,
+		error,
+		sendRequest,
+	};
+};
+
+export default useHttp;
